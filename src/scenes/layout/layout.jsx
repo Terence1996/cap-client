d8db99e30d8026d8f2aa76b65c5e6ab1dc763ec4
Terence1996 <you@example.com>
2023-08-13 18:31:48 +0530

    first commit

---
+++ src/scenes/layout/layout.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react'
+import {Box, useMediaQuery} from '@mui/material';
+import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux/es/hooks/useSelector';
+import { Navbar } from '../../components/Navbar';
+import { Sidebar } from '../../components/Sidebar';
+import { useGetUserQuery } from '../../state/api.js';
+
+
+
+ const Layout = () => {
+const [layout, setLayout] = useState({})
+const isNonMobile = useMediaQuery("(min-width:600px)");
+const [isSidebarOpen, setIsSidebarOpen]= useState(true);
+const userId = useSelector ((state)=>state.global.userId);
+console.log(userId)
+const {data, refetch} = useGetUserQuery(userId);
+useEffect(()=>{
+refetch();
+setLayout(data)
+},[])
+console.log("data", data);
+
+  return (
+   <Box display={isNonMobile?"flex":"block"} width="100%" height="100%">
+    <pre>
+      {JSON.stringify(layout)}
+    </pre>
+    <Sidebar
+    user={ data || {}}
+    isNonMobile={isNonMobile}
+    drawerWidth="250px"
+    isSidebarOpen={isSidebarOpen}
+    setIsSidebarOpen={setIsSidebarOpen}
+    />
+<Box>
+ <Navbar 
+//  user={ data || {}}
+ isSidebarOpen={isSidebarOpen}
+ setIsSidebarOpen={setIsSidebarOpen}
+ />
+  <Outlet/>
+    </Box>
+   </Box>
+  )
+}
+
+export default Layout;
---
+++ src/state/index.js
@@ -0,0 +1,22 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+
+const initialState = {
+    mode:"dark",
+    userId: "63701cc1f032398675000120",
+}
+
+export const globalSlice = createSlice({
+    name:"global",
+    initialState,
+    reducers:{
+        setMode:(state)=>{
+            state.mode = state.mode === 'light'?'dark':'light';
+
+        }
+    }
+})
+
+export const {setMode} = globalSlice.actions;
+
+ export default globalSlice.reducer;
